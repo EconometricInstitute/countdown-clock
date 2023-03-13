14947ad47b3ff5d26270d0a299eced4ed2617f71
Paul Bouman <bouman@ese.eur.nl>
2023-03-13 13:05:34 +0100

    Setup workflow for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/countdown-clock/' : '/',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
